0f1a6bc46530d6e3e7c4b3ce244bfce269126776
Yousif-hash020 <arainyousif020@gmail.com>
2026-08-14 23:24:07 +0500

    feat: update guest profile functionality, enhance data fetching, and improve form submission handling

--- Frontend/Guest/guest-profile.js
+++ Frontend/Guest/guest-profile.js
@@ -1,3 +1,14 @@
+let guestName = document.querySelector(".user-name-title");
+let guestEmail = document.querySelector(".user-email-subtitle");
+let form = document.querySelector(".form-grid-2");
+let formName = document.querySelector("#first-name");
+let phone = document.querySelector("#phone-num");
+let cnic = document.querySelector("#cnic-id");
+let city = document.querySelector("#user-city");
+let bio = document.querySelector("#bio-text");
+let savebtn = document.querySelector(".save-btn");
+
+
 const token = localStorage.getItem("token");
 const user = JSON.parse(localStorage.getItem("user") || "{}");
 
@@ -6,11 +17,10 @@ if (!token || !user || user.role !== "guest") {
 }
 
 
-
 let getProfile = async () => {
     try {
         const token = localStorage.getItem("token");
-        const response = await fetch("http://localhost:5000/api/rooms",
+        const response = await fetch("http://localhost:5000/api/auth/me ",
             {
                 method: "GET",
                 headers: {
@@ -18,10 +28,54 @@ let getProfile = async () => {
                 }
             }
         );
-    
-    const data = await response.json();
-    }catch(error){
+
+        const data = await response.json();
+        if (guestName) {
+            guestName.textContent = data.user.name;
+            guestName.style.textTransform = 'capitalize';
+        }
+        if (guestEmail) {
+            guestEmail.textContent = data.user.email;
+        };
+    } catch (error) {
         console.error(error)
     }
-
 };
+
+let updateProfile = async () => {
+    try {
+
+        form.addEventListener("submit", async (e) => {
+            e.preventDefault();
+            const response = await fetch("http://localhost:5000/api/guest/profile", {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`
+                },
+                body: JSON.stringify({
+                    name: formName.value,
+                    phone: phone.value,
+                    cnic: cnic.value,
+                    city: city.value,
+                    bio: bio.value
+                })
+
+            });
+            const data = await response.json();
+            console.log(data);
+            await getProfile();
+            form.reset();
+        });
+
+
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+savebtn.addEventListener("click", async () => {
+    await updateProfile();
+})
+getProfile();
