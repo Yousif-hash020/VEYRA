6deda078e978014cbc6ed5307721863139f83388
Yousif-hash020 <arainyousif020@gmail.com>
2026-08-16 23:57:29 +0500

    feat: add script inclusion for guest property detail page and implement property details fetching logic

--- Frontend/Guest/guest-property-detail.js
+++ Frontend/Guest/guest-property-detail.js
@@ -4,3 +4,21 @@ const user = JSON.parse(localStorage.getItem("user") || "{}");
 if (!token || !user || user.role !== "guest") {
     window.location.href = "/Frontend/auth.html";
 }
+
+
+let getDetails= async ()=>{
+    try {
+        const response = await fetch("http://localhost:5000/api/guest/properties/:id", {
+            method:"GET",
+            headers:{
+                "Authorizzation": `Bearer ${token}`
+            }
+        });
+
+        const data = await response.json();
+
+        console.log(data);
+    } catch (error) {
+        console.error(error, "Error")
+    }
+}
